e0938c8a1822ada7b28f0033e1ebd147bc47af77
martynaswiercz <mswierczm1@gmail.com>
2023-01-22 16:32:17 +0100

    Liczba scian w modelu

--- ifc_wall.py
+++ ifc_wall.py
@@ -1,2 +1,6 @@
 import ifcopenshell.util.element
 import pprint as pp
+
+m = ifcopenshell.open("231110AC-11-Smiley-West-04-07-2007.ifc")
+walls = m.by_type("IfcWall")
+print(f'Liczba ścian w modelu: {len(walls)}')
