a1a4470a9d0bb047987a16f4d0d70216b28301d6
martynaswiercz <mswierczm1@gmail.com>
2023-01-22 16:33:42 +0100

    Sciany zewnetrzne

--- ifc_wall.py
+++ ifc_wall.py
@@ -4,3 +4,29 @@ import pprint as pp
 m = ifcopenshell.open("231110AC-11-Smiley-West-04-07-2007.ifc")
 walls = m.by_type("IfcWall")
 print(f'Liczba ścian w modelu: {len(walls)}')
+
+walls = m.by_type("IfcWall")
+wall = walls[1]
+pset_for_wall = ifcopenshell.util.element.get_psets(wall)
+pp.pprint(pset_for_wall)
+
+ext_walls = []
+for w in walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    if psets.get("Pset_WallCommon"):
+        if bool(psets.get("Pset_WallCommon").get("IsExternal")):
+            ext_walls.append(w)
+
+print(f'Liczba ścian zewnętrznych: {len(ext_walls)}')
+
+# objetosc scian zewnetrznych
+totalvolume = 0
+
+for w in ext_walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    for psetname, pset_dict in psets.items():
+        for name, value in pset_dict.items():
+            # print (f"{name}: {value}")
+            if name == "NetVolume":
+                totalvolume += float(value)
+print(f'TotalVolume: {totalvolume:.2f}')
